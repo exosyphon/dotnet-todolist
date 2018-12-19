c8129b75cc3e3b6b9caee9118ebd2601683244f1
Andrew Courter <exosyphon@yahoo.com>
2018-12-19 15:04:34 -0600

    Add fake test for Model

---
+++ DotNetReact.Tests/Models/TodoItemTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace DotNetReact.Tests.Models
+{
+    [TestFixture]
+    public class TodoItemTest
+    {
+        [Test]
+        public void ShouldDoThings()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
